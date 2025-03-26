d17647484ecd12c563841799e6dd439129c9f2c8
Yakubu Abdulhameed <yakubuabdulhameedugbede@gmail.com>
2025-03-26 17:04:31 +0100

    Setting up the fonts, Themes and Icons

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Login } from './Screens/Login'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
 
 export default function App() {
     return (
         <View>
-            <Login />
+            <FontAwesomeIcon icon="fa-solid fa-church" />
             <Text>App</Text>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'roboto',
+    },
+    text: {
+        color: 'black',
+        fontSize: 24,
+        fontFamily: Themes.fonts.header,
+    }
+})
---
+++ FrameWork/Components/Themes.js
@@ -0,0 +1,15 @@
+export const Themes = {
+    colours: {
+        primary: '#000000',
+        secondary: '#ffffff',
+        tertiary: '#ff0000',
+        quaternary: '#00ff00',
+        quinary: '#0000ff',
+    },
+    fonts: {
+        header: 'roboto_regular_400',
+        primary2: 'roboto_regular_300',
+        secondary: 'roboto_bold',
+
+    }
+}
